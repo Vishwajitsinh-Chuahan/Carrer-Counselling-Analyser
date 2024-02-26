fefe1c32ff218b5a2de3f4c20636621101047c38
Vasu <129766426+VasuBhalani@users.noreply.github.com>
2024-02-26 21:31:59 +0530

    Update insertstudentdetails.php
    inserting data into database

--- insertstudentdetails.php
+++ insertstudentdetails.php
@@ -3,7 +3,7 @@ include('connection.php');
 if(isset($_POST['submit']))
 {
   $full_name     =$_POST['fullname'];
-  $student_id    =$_POST['idnumber'];
+  $student_id    =$_SESSION['account'];
   $contact       =$_POST['contactnumber'];
   $email         =$_POST['email'];
   $hsc           =$_POST['result_hsc'];
@@ -24,6 +24,7 @@ if(isset($_POST['submit']))
   $year3          =$_POST['year3'];
   $year4          =$_POST['year4'];
   $interest        =$_POST['interest'];
+  $counsellor      =$_POST['counsellor'];
   
   $cp              =$_POST['cp'];
   $web               =$_POST['web'];
@@ -40,10 +41,10 @@ if(isset($_POST['submit']))
   $linkedin_link=$_POST['link'];
 
       
-    $query1="INSERT INTO personalinformation VALUES('$full_name','$student_id','$contact','$email','$hsc','$medium','$meritrank','$admission','$currentsem','$sem1','$sem2','$sem3','$sem4','$sem5','$sem6','$sem7','$sem8','$year1','$year2','$year3','$year4','$interest' )";
+    $query1="INSERT INTO personalinformation (fullname,student_id,contactnumber,email,result_hsc,medium,meritrank,admissiontype,currentsemester,sem1,sem2,sem3,sem4,sem5,sem6,sem7,sem8,year1,year2,year3,year4,interest,counsellor) VALUES('$full_name','$student_id','$contact','$email','$hsc','$medium','$meritrank','$admission','$currentsem','$sem1','$sem2','$sem3','$sem4','$sem5','$sem6','$sem7','$sem8','$year1','$year2','$year3','$year4','$interest','$counsellor' )";
     $data=mysqli_query($conn,$query1);
 
-    $query2 = "INSERT INTO technologystand (cp,web,ai,android,otherfield,any_score,prefer,idnumber) VALUES ('$cp','$web','$ai','$android','$otherfield','$any_score','$prefer','$student_id')";
+    $query2 = "INSERT INTO technologystand (cp,web,ai,android,otherfield,any_score,prefer,student_id) VALUES ('$cp','$web','$ai','$android','$otherfield','$any_score','$prefer','$student_id')";
     $data=mysqli_query($conn,$query2);
   
   foreach($_POST['hlanguages'] as $key => $value)
@@ -53,7 +54,7 @@ if(isset($_POST['submit']))
       $h_points = mysqli_real_escape_string($conn, $_POST['hpoint'][$key]);
       $h_number = mysqli_real_escape_string($conn, $_POST['hnumber'][$key]);
 
-      $hquery = "INSERT INTO hackerrank (hlanguages,hrank,hpoint, hnumber,idnumber) VALUES ('$h_languages', '$h_rank', '$h_points', '$h_number','$student_id')";
+      $hquery = "INSERT INTO hackerrank (hlanguages,hrank,hpoint, hnumber,student_id) VALUES ('$h_languages', '$h_rank', '$h_points', '$h_number','$student_id')";
       $query_run = mysqli_query($conn, $hquery);
   }
 
@@ -64,7 +65,7 @@ if(isset($_POST['submit']))
       $c_points = mysqli_real_escape_string($conn, $_POST['cpoint'][$key]);
       $c_number = mysqli_real_escape_string($conn, $_POST['cnumber'][$key]);
 
-      $cquery = "INSERT INTO codechef (clanguages,crank,cpoint, cnumber,idnumber) VALUES ('$c_languages', '$c_rank', '$c_points', '$c_number','$student_id')";
+      $cquery = "INSERT INTO codechef (clanguages,crank,cpoint, cnumber,student_id) VALUES ('$c_languages', '$c_rank', '$c_points', '$c_number','$student_id')";
       $query_run = mysqli_query($conn, $cquery);
   }
  
@@ -75,7 +76,7 @@ if(isset($_POST['submit']))
       $l_points = mysqli_real_escape_string($conn,$_POST['lpoint'][$key]);
       $l_number = mysqli_real_escape_string($conn,$_POST['lnumber'][$key]);
 
-      $lquery = "INSERT INTO leetcode (languages,lrank,lpoint, lnumber,idnumber)VALUES ('$l_languages', '$l_rank', '$l_points', '$l_number','$student_id')";
+      $lquery = "INSERT INTO leetcode (languages,lrank,lpoint, lnumber,student_id)VALUES ('$l_languages', '$l_rank', '$l_points', '$l_number','$student_id')";
       $query_run = mysqli_query($conn, $lquery);
   }
 
@@ -85,7 +86,7 @@ if(isset($_POST['submit']))
       $nptel_courses = mysqli_real_escape_string($conn, $value);
       $nptel_score = mysqli_real_escape_string($conn,$_POST['nscore'][$key]);
 
-      $nptelquery = "INSERT INTO nptel (nptelcourses,nscore,idnumber) VALUES ('$nptel_courses', '$nptel_score','$student_id')";
+      $nptelquery = "INSERT INTO nptel (nptelcourses,nscore,student_id) VALUES ('$nptel_courses', '$nptel_score','$student_id')";
       $query_run = mysqli_query($conn, $nptelquery);
   }
 
@@ -94,7 +95,7 @@ if(isset($_POST['submit']))
       $professional_courses = mysqli_real_escape_string($conn, $value);
       $p_result = mysqli_real_escape_string($conn,$_POST['result'][$key]);
 
-      $coursequery = "INSERT INTO professionalcourses (course,result,idnumber) VALUES ('$professional_courses', '$p_result','$student_id')";
+      $coursequery = "INSERT INTO professionalcourses (course,result,student_id) VALUES ('$professional_courses', '$p_result','$student_id')";
       $query_run = mysqli_query($conn, $coursequery);
   }
 
@@ -103,11 +104,11 @@ if(isset($_POST['submit']))
       $project_name = mysqli_real_escape_string($conn, $value);
       $tech_name = mysqli_real_escape_string($conn,$_POST['tech'][$key]);
 
-      $projectquery = "INSERT INTO research (project,tech,idnumber) VALUES ('$project_name', '$tech_name','$student_id')";
+      $projectquery = "INSERT INTO research (project,tech,student_id) VALUES ('$project_name', '$tech_name','$student_id')";
       $query_run = mysqli_query($conn, $projectquery);
   }
 
-    $githubquery="INSERT INTO github (githubprojectrepo,contributions,githubstars,link,idnumber) VALUES ('$repo','$contributions','$stars','$linkedin_link','$student_id')";
+    $githubquery="INSERT INTO github (githubprojectrepo,contributions,githubstars,link,student_id) VALUES ('$repo','$contributions','$stars','$linkedin_link','$student_id')";
     $query_run = mysqli_query($conn,$githubquery);
 
   if($query_run==true&&$data==true)

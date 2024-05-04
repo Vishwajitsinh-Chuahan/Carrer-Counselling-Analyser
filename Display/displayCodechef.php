5cbed34a547ce5cb4b4d91deb5dffc5c634e0ce1
Vasu <129766426+VasuBhalani@users.noreply.github.com>
2024-05-04 23:43:51 +0530

    Create displayCodechef.php

---
+++ Display/displayCodechef.php
@@ -0,0 +1,48 @@
+<?php
+session_start();
+if (!(isset($_SESSION['logedin'])) || $_SESSION['logedin'] != true) {
+    header('Location:index.php');
+    exit; // Added exit to stop further execution
+} else {
+    include('connection.php');
+    // $id = $_GET['id'];
+}
+?>
+<?php
+include('connection.php');
+
+if(isset($_POST['displaySend'])) {
+    $table='<table class="table">
+    <thead class="thead" style="background-color: lightgrey;">
+      <tr>
+        <th scope="col">Sr.No</th>
+        <th scope="col">Language</th>
+        <th scope="col">Rank</th>
+        <th scope="col">Points</th>
+        <th scope="col">No. of Problems</th>
+      </tr>
+    </thead>';
+
+    $sql='select * from codechef where student_id="'.$_SESSION['account'].'"';
+    $result=mysqli_query($conn,$sql);
+    $i=1;
+    while($row = mysqli_fetch_assoc($result)) {
+        $language=$row['clanguages'];
+        $rank=$row['crank'];
+        $point=$row['cpoint'];
+        $number=$row['cnumber'];
+
+        $table.=' <tr>
+        <td scope="row">'.$i.'</td>
+        <td>'.$language.'</td>
+        <td>'.$rank.'</td>
+        <td>'.$point.'</td>
+        <td>'.$number.'</td>
+      </tr>';
+      
+      $i++;
+}
+       $table.='</table>';
+       echo $table;
+}
+?>

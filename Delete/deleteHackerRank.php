6a1f29ff2d9378526044477f54860a83bbeec056
Vasu <129766426+VasuBhalani@users.noreply.github.com>
2024-05-06 13:54:54 +0530

    Add files via upload

---
+++ Delete/deleteHackerRank.php
@@ -0,0 +1,21 @@
+<?php
+session_start();
+if (!(isset($_SESSION['logedin'])) || $_SESSION['logedin'] != true) {
+    header('Location:index.php');
+    exit; // Added exit to stop further execution
+} else {
+    include('connection.php');
+    // $id = $_GET['id'];
+}
+?>
+<?php
+include('connection.php');
+
+if(isset($_POST['deleteSend'])) {
+    $sr_no = $_POST['deleteSend'];
+
+    $sql = "DELETE FROM hackerrank WHERE sr_no =$sr_no";
+    $result=mysqli_query($conn,$sql);
+
+}    
+?>
